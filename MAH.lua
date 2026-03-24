9835237743b361a1e40fa6dff5cc71143e1c6b4f
KENN <157271873+KENTENN@users.noreply.github.com>
2026-03-24 23:20:44 +0700

    Update and rename README.md to MAH.lua

---
+++ MAH.lua
@@ -0,0 +1,13 @@
+repeat task.wait() until game:IsLoaded(3)
+local LV = workspace.Camera.asdzxccg_0248.Head.NameLevelBBGUI.LevelFrame.TextLabel.Text
+local level = tonumber(string.match(LV,"%d+"))
+local Gold = game:GetService("Players").LocalPlayer.PlayerGui.HUD.BottomFrame.CurrencyList.Coins.Amount.Text
+local Gems = game:GetService("Players").LocalPlayer.Backpack.Framework.TasksV2.TaskCard.TaskCardTemplate.ClaimButton.GemsAmount.Text
+local stardust =  game:GetService("Players").LocalPlayer.PlayerGui.HUD.BottomFrame.CurrencyList.Stardust.Amount.Text
+
+local message = "👾Level :" .. level ..  "    🪙Gold :" .. Gold .. "    💎Gems :" .. Gems .. "    ⭐stardust :"  .. stardust
+
+while true do
+_G.Horst_SetDescription(message)
+task.wait(5)
+end
